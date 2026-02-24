1d85625298893dad3b71a6f5e9de83e8ad2945f0
huok <77536370+vraestoren@users.noreply.github.com>
2026-02-24 12:50:55 +0300

    Update random_d_uk.py

--- src/random_d_uk.py
+++ src/random_d_uk.py
@@ -1,13 +1,13 @@
 from os import getcwd
 from time import time
 from pathlib import Path
-from requests import get
-
+from requests import Session
 
 class RandomDuk:
 	def __init__(self) -> None:
 		self.api = "https://random-d.uk/api"
-		self.headers = {
+		self.session = Session()
+		self.session.headers = {
 			"user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/97.0.4692.99 Safari/537.36"
 		}
 	
@@ -16,34 +16,24 @@ class RandomDuk:
 			content: bytes,
 			location: str = getcwd()) -> bool:
 		with open(
-			Path(location).joinpath(f"{time() * 1000}.jpg"),
-		mode="wb+",
-		) as file:
-			file.write(content)
-			file.close()
+			Path(location).joinpath(f"{time() * 1000}.jpg"), mode="wb+") as file:
+				file.write(content)
+				file.close()
 		return True
 
 	def get_random_image_url(self) -> dict:
-		return get(
-			f"{self.api}/random",
-			headers=self.headers).json()
+		return self.session.get(f"{self.api}/random").json()
 	
 	def get_random_image(self) -> bool:
-		return save_file(get(
-			f"{self.api}/random",
-			headers=self.headers).content)
+		return save_file(self.session.get(f"{self.api}/random").content)
 	
 	def get_all_images(self) -> dict:
-		return get(
-			f"{self.api}/list",
-			headers=self.headers).json()
+		return self.session.get(f"{self.api}/list").json()
 	
 	def get_image(self, image_number: int) -> bool:
-		return save_file(get(
-			f"{self.api}/{image_number}.jpg",
-			headers=self.headers).content)
+		return save_file(self.session.get(
+			f"{self.api}/{image_number}.jpg").content)
 	
 	def get_gif(self, gif_number: int) -> bool:
-		return save_file(get(
-			f"{self.api}/{gif_number}",
-			headers=self.headers).content)
+		return save_file(self.session.get(
+			f"{self.api}/{gif_number}").content)
